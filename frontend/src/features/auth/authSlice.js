44f0c297b1f4aa00cdd3f0b2f887c4dbb0f525bb
Nate Rodrigues <naterodrigues@Nates-MacBook-Pro.local>
2022-01-29 23:03:48 +1100

    Initial commit

---
+++ frontend/src/features/auth/authSlice.js
@@ -0,0 +1,71 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import authService from './authService';
+
+// Get user from localstorage
+const user = JSON.parse(localStorage.getItem('user'));
+
+const initialState = {
+  user: null,
+  isError: false,
+  isSuccess: false,
+  isLoading: false,
+  message: '',
+};
+
+// Register new user
+export const register = createAsyncThunk(
+  'auth/register',
+  async (user, thunkAPI) => {
+    try {
+      return await authService.register(user);
+    } catch (error) {
+      console.log(error.message);
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+      console.log(message);
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
+
+// Login new user
+export const login = createAsyncThunk('auth/login', async (user, thunkAPI) => {
+  console.log(user);
+});
+
+export const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {
+    reset: (state) => {
+      state.isLoading = false;
+      state.isError = false;
+      state.isSuccess = false;
+      state.message = '';
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(register.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(register.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.user = action.payload;
+      })
+      .addCase(register.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload; // We know the message is in the payload.
+        state.user = null;
+      });
+  },
+});
+
+export const { reset } = authSlice.actions; // When you create an action within a reducer you export it like
+export default authSlice.reducer;
